a151db848cc910c47d423a52a775259e8b514023
Elizaveta <elizaveta.ermakovich@tuni.fi>
2026-01-19 17:43:27 +0100

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import Global Styles
 import './styles/index.css'; 
