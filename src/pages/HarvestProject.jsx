19fee5c158d66f128687d34b177f201263722afa
Elizaveta <elizaveta.ermakovich@tuni.fi>
2026-01-19 17:35:23 +0100

    test

--- src/pages/HarvestProject.jsx
+++ src/pages/HarvestProject.jsx
@@ -16,7 +16,7 @@ const HarvestProject = () => {
       
     <div className="hero-section">
       <div className="hero-text">
-        <h1 className="hero-title">Harvest</h1>
+        <h1 className="hero-title">Haaaarvest</h1>
         <p className="hero-subtitle">A delivery app that makes seasonal & local eating effortless</p>
       </div>
       <img className="hero-image" src={HarvestImages.hero} alt="Hero image for Harvest project" />
